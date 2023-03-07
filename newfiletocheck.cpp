dabbc06539eb57f6a3ac15927c762417f5e03026
Kashshaf-Labib <labibkashshaf@gmail.com>
2023-03-07 08:50:54 +0600

    New file after modifying

---
+++ newfiletocheck.cpp
@@ -0,0 +1,12 @@
+/*“So, be patient. Surely Allah’s promise is true, and let not the disbelievers shake your firmness” (Quran, 30:60)*/
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(0);
+    cin.tie(0);
+    cout << "New file" << endl;
+
+    return 0;
+}
