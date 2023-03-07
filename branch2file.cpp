9d2d8612f357e044ae466e23ced77fa945a9f59e
Kashshaf-Labib <labibkashshaf@gmail.com>
2023-03-07 19:18:54 +0600

    A new file called branch2file has been created in branch2

---
+++ branch2file.cpp
@@ -0,0 +1,12 @@
+/*“So, be patient. Surely Allah’s promise is true, and let not the disbelievers shake your firmness” (Quran, 30:60)*/
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base ::sync_with_stdio(0);
+    cin.tie(0);
+    cout << "This file ahs been created in branch2" << endl;
+
+    return 0;
+}
